760936e226f1788e229d1440b1b0827385d64183
Charles O'Farrell <charleso@charleso.org>
2011-06-02 20:11:58 +1000

    Change the site url to blog instead of blog2

--- _config.py
+++ _config.py
@@ -19,7 +19,7 @@
 #  If you're hosting a blogofile powered site as a subdirectory of a larger
 #  non-blogofile site, then you would set the site_url to the full URL
 #  including that subdirectory: "http://www.yoursite.com/path/to/blogofile-dir"
-site.url = "http://blog2.charleso.org"
+site.url = "http://blog.charleso.org"
 
 #### Blog Settings ####
 blog = controllers.blog
